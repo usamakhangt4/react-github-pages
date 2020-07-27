37eaeab050040595cb7ba66fef6550190283a67a
Muhammad Usama Bin Akhtar Khan <usamakhangt4@gmail.com>
2020-07-27 19:28:58 +0500

    Updates

---
+++ precache-manifest.5a843fd7f361fe2fb281ea03f6742d82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae46c1a0bff758d3eb62b3e5f8ba51be",
+    "url": "/react-github-pages/index.html"
+  },
+  {
+    "revision": "9f7a4195b136f1601c74",
+    "url": "/react-github-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "89279df726285becfc19",
+    "url": "/react-github-pages/static/js/2.a9c19741.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-github-pages/static/js/2.a9c19741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f7a4195b136f1601c74",
+    "url": "/react-github-pages/static/js/main.68b7e40d.chunk.js"
+  },
+  {
+    "revision": "549506727088d589a102",
+    "url": "/react-github-pages/static/js/runtime-main.cdc1f5da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-github-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fb6a0867c9fe802f0ad545590edcfff2.js"
+  "/react-github-pages/precache-manifest.5a843fd7f361fe2fb281ea03f6742d82.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-github-pages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
